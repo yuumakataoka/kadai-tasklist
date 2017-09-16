35a8817ba25bc3d72e8d6493801bc34cb644f294
eimyver3 <yuuma2.index@gmail.com>
2017-09-16 08:06:35 +0000

    完成

--- db/migrate/20170914134043_create_tasks.rb
+++ db/migrate/20170914134043_create_tasks.rb
@@ -1,5 +1,9 @@
 class CreateTasks < ActiveRecord::Migration[5.0]
   def change
-      add_column :tasks, :status, :string
+    create_table :tasks do |t|
+      t.string :content
+
+      t.timestamps
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170914134043) do
+ActiveRecord::Schema.define(version: 20170916074608) do
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "task"
+    t.string   "status"
   end
 
 end
